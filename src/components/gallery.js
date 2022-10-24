0b58334f1a4e51eca9686bcc5ca8c4ea54056aba
raavann <imraavann@gmail.com>
2022-10-25 02:47:17 +0530

    fixes

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,6 +3,11 @@ function Gallery(){
   return (
     <div id="gallery" className="snap-x full-screen h-full w-screen">
       <div className="demo-wrapper [&_img]:w-full [&_img]:h-auto [&_img]:bg-[#f0f0f0] [&_ul]:pl-4 [&_ul]:list-none [&_li]:shrink-0 [&_li]:pr-4 overflow-x-hidden">
+        
+      <div className="w-screen h-screen flex justify-center items-center">
+          <h1 className="text-[#d6d6d6] text-9xl font-bold">Gallery</h1>
+      </div>
+
         <section className="demo-text">
           <div className="wrapper text">#BEFLICKINAWESOME#BEFLIC</div>
         </section>
--- src/components/svg/svgCircles.js
+++ src/components/svg/svgCircles.js
@@ -2,7 +2,7 @@
 function SvgCircle(props){
   return (
     <svg className="circle absolute -z-50 w-full h-full">
-      <circle cx={props.x} cy={props.y} r={props.r} fill="none" stroke="#949494"/>
+      <circle cx={props.x} cy={props.y} r={props.r} fill="none" stroke="#646464" strokeWidth={"1px"}/>
     </svg>
 );
 }
